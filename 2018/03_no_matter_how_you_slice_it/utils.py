1d965063c4a3735dfb565a5882a5de18a225b6b3
Tom <tafgreen1@sheffield.ac.uk>
2022-11-10 11:53:34 +0000

    solution to day 2 in old format, sol for day 3 in new ipynb format

---
+++ 2018/03_no_matter_how_you_slice_it/utils.py
@@ -0,0 +1,27 @@
+def load_puzzle_input():
+    
+    with open('input.txt', 'r') as infile: 
+    
+        lines = infile.read().splitlines()
+        return [line for line in lines if line]
+    
+def test_code(func, example_dict):
+    
+    failed = False
+    
+    for sample_index, (sample_input, sample_output) in enumerate(example_dict.items()):
+        
+        if func(sample_input) == sample_output:
+            
+            print(f"Test {sample_index} passed: Input <{sample_input}> gives output <{sample_output}>.")
+            
+        else:
+            
+            print(f"Test {sample_index} failed: Input <{sample_input}> expected <{sample_output}>, but got <{func(sample_input)}>.")
+            failed = True
+            
+    print()
+    if failed: 
+        print("Didn't get it this time... Try again! You got this!")
+    else:
+        print("Congratulations! Looks like you cracked it! Good job!")        
