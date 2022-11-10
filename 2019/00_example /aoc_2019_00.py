0c7c4e178f1e51da748737ace3de0afa3a891e68
Tom <tafgreen1@sheffield.ac.uk>
2022-11-10 11:53:14 +0000

    template & solution for day 1 for 2019

---
+++ 2019/00_example /aoc_2019_00.py
@@ -0,0 +1,53 @@
+import pathlib
+import sys
+
+import itertools, numpy # extra
+
+def parse(puzzle_input):
+
+    """Parse input"""
+    
+    return [int(line) for line in puzzle_input.split("\n")] 
+
+
+def part1(data):
+
+    """Solve part 1"""
+    
+    for combination in itertools.combinations(data, 2):
+        
+        if sum(combination) == 2020:
+            
+            return numpy.product(combination)
+
+
+def part2(data):
+
+    """Solve part 2"""
+
+
+def solve(puzzle_input):
+
+    """Solve the puzzle for the given input"""
+
+    data = parse(puzzle_input)
+
+    solution1 = part1(data)
+
+    solution2 = part2(data)
+
+
+    return solution1, solution2
+
+
+if __name__ == "__main__":
+
+    for path in sys.argv[1:]:
+
+        print(f"{path}:")
+
+        puzzle_input = pathlib.Path(path).read_text().strip()
+
+        solutions = solve(puzzle_input)
+
+        print("\n".join(str(solution) for solution in solutions))
---
+++ 2019/XX_template/test_aoc_2019_XX.py
@@ -0,0 +1,62 @@
+import pathlib
+import pytest
+
+#################
+# CHANGE [XX] TO DAY
+import aoc_2019_XX as aoc 
+#################
+
+PUZZLE_DIR = pathlib.Path(__file__).parent
+
+
+@pytest.fixture
+def example1():
+
+    puzzle_input = (PUZZLE_DIR / "example1.txt").read_text().strip()
+    return aoc.parse(puzzle_input)
+
+
+@pytest.fixture
+def example2():
+
+    puzzle_input = (PUZZLE_DIR / "example2.txt").read_text().strip()
+    return aoc.parse(puzzle_input)
+
+#################
+
+#################
+def test_parse_example1(example1):
+
+    """Test that input is parsed properly"""
+
+    #################
+    # ADD EXAMPLE 1 PARSED FORM HERE
+    assert example1 == 2
+    #################
+    
+#################
+# DELETE WHEN USED
+@pytest.mark.skip(reason="Not implemented") 
+#################
+def test_part1_example1(example1):
+
+    """Test part 1 on example input"""
+
+    #################
+    # ADD EXAMPLE 1 SOLUTION HERE
+    assert aoc.part1(example1) == ... 
+    #################
+
+
+#################
+# DELETE WHEN USED
+@pytest.mark.skip(reason="Not implemented") 
+#################
+def test_part2_example2(example2):
+
+    """Test part 2 on example input"""
+
+    #################
+    # ADD EXAMPLE 2 SOLUTION HERE
+    assert aoc.part2(example2) == ... 
+    #################
